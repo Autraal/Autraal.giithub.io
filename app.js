76be9a82e911447888662a7be94d8c4a534278d2
autraal <okikioluwaolumide@gmail.com>
2022-07-18 13:33:01 -0200

    added

--- app.js
+++ app.js
@@ -24,3 +24,10 @@ AOS.init({
     duration: 2100,
   })
   
+
+  
+const fadeOut = ()=>{
+    const loaderWrapper = document.querySelector('.wrapper');
+    loaderWrapper.classList.add('fade');
+    }
+    window.addEventListener('load',fadeOut)
