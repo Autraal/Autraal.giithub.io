ca90a005d0151e157b0c5449bc9a210490e65ace
autraal <okikioluwaolumide@gmail.com>
2022-07-20 15:54:16 -0200

    fixed background image issue

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+// hambrger 
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobileMenu= document.querySelector('.header .nav-bar .nav-list ul');
 const menuItem= document.querySelectorAll('.header .nav-bar .nav-list ul li a');
@@ -11,21 +14,22 @@ hamburger.addEventListener('click', ()=>{
 
 
 
-menuItem.forEach(item=>{
+menuItem.forEach(
+    item=>{
     item.addEventListener('click', ()=>{
         hamburger.classList.toggle('active');
         mobileMenu.classList.toggle('active');  
     })
 })
 
-
+// aos animations
 
 AOS.init({
     duration: 2100,
   })
   
 
-  
+//   pre loader
 const fadeOut = ()=>{
     const loaderWrapper = document.querySelector('.wrapper');
     loaderWrapper.classList.add('fade');
